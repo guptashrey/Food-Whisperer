ba7fb620bf6bc92cbb0f96c5eab254a071090a58
Shrey Gupta <shrey.gupta1904@gmail.com>
2023-04-09 02:47:01 +0000

    Added FastAPI server

---
+++ scripts/inference_server.py
@@ -0,0 +1,22 @@
+# library imports
+from fastapi import FastAPI
+from inference import get_recommended_recipes
+# initialize fastapi app
+app = FastAPI()
+
+# define search endpoint
+@app.get("/search/{query}")
+async def search(query: int):
+    '''
+    Search endpoint for searching reddit posts and research papers.
+    Args:
+        query (str): Query string.
+    Returns:
+        dict: Dictionary containing search results.
+    '''
+
+    # run qna pipeline on reddit posts
+    results = get_recommended_recipes([query])
+
+    # return search results
+    return results.to_dict(orient="records")
