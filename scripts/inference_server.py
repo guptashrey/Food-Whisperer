a4fc48a0f6898bb423c4ca7052454aaeeadc63d9
Shrey Gupta <shrey.gupta1904@gmail.com>
2023-04-09 19:50:50 +0000

    Added authentication in API

--- scripts/inference_server.py
+++ scripts/inference_server.py
@@ -1,9 +1,35 @@
 # library imports
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.responses import JSONResponse
 from inference import get_recommended_recipes
+import json
 
 # initialize fastapi app
 app = FastAPI()
+
+# load the api key from config.json
+config = json.load(open("../config.json"))
+api_key = config["api-key"]
+
+# define middleware for authentication
+@app.middleware("http")
+async def authentication(request: Request, call_next):
+    '''
+    Middleware for authentication.
+    Args:
+        request (Request): Request object.
+        call_next (function): Function to call next.
+    Returns:
+        JSONResponse: JSON response object.
+    '''
+    if request.headers.get('api-key') == api_key:
+        response = await call_next(request)
+
+    else:
+        response = JSONResponse(
+            content={"message": "unauthorized access"}, status_code=401
+        )
+    return response
 
 # define recommendation endpoint
 @app.get("/search/{query}")
